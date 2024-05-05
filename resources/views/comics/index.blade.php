7e6a35325bb4ca363e2959f3736ff933130464c9
yukirinmaru <ynishida1225@gmail.com>
2024-05-05 21:48:06 +0900

    投稿一覧表示

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Review</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Blog Name</h1>
+        <div class='comics'>
+            @foreach ($comics as $comic)
+                <div class='comic'>
+                    <h2 class='name'>{{ $comic->name }}</h2>
+                    <p class='overview'>{{ $comic->overview }}</p>
+                </div>
+            @endforeach
+        </div>
+        <div class='paginate'>
+            {{ $comics->links() }}
+        </div>
+    </body>
+</html>
